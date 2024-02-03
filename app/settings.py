8ab4a04c1d96ce29efbbdd651e2df11e2cc898e9
LeGodurix <hugoorickx@gmail.com>
2024-02-03 23:18:08 +0100

    blabla

--- app/settings.py
+++ app/settings.py
@@ -16,6 +16,8 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_WHITELIST = ["*"]
+CSRF_TRUSTED_ORIGINS = ['*']
 # AUTH_USER_MODEL = 'app.CustomUser'
 INSTALLED_APPS = [
     'django.contrib.admin',
